5768bf0df22193dc3b625e2da256cd5d8749772a
Daniel Hutmacher <daniel@strd.co>
2026-01-14 10:43:50 +0100

    Plaintext, including the URL in <link> must be encoded

--- server.js
+++ server.js
@@ -897,7 +897,7 @@ async function getRssFeed(req, res, next) {
 
                     items+='<item>\n'+
                             '<title>'+encodeHtml(item.EventName)+'</title>\n'+
-                            '<link>'+item.URL+(item.URL.toLowerCase().indexOf('utm_source')==-1 ? (item.URL.indexOf('?')==-1 ? '?' : '&')+'utm_source=callfordataspeakers&utm_campaign=rss-feed' : '')+'</link>\n'+
+                            '<link>'+encodeHtml(item.URL+(item.URL.toLowerCase().indexOf('utm_source')==-1 ? (item.URL.indexOf('?')==-1 ? '?' : '&')+'utm_source=callfordataspeakers&utm_campaign=rss-feed' : ''))+'</link>\n'+
                             '<dc:creator>Call for Data Speakers</dc:creator>\n'+
                             (item.Created ? '<pubDate>' + item.Created.toUTCString() + '</pubDate>\n' : '')+
                             '<category>Call for Speakers</category>\n'+
